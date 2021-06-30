0bca20319315c751ee9192ad0f71730cfe32b1e3
Diego Cardoso <diegocar448@hotmail.com>
2021-06-30 17:06:04 -0300

    upFiles

---
+++ src/main/java/com/azure_spring_cloud/azure_spring_cloud/AzureSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.azure_spring_cloud.azure_spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringCloudApplication.class, args);
+	}
+
+}
